e36a415b5d9e28feb3d504a3539cb6cdc180449a
Ibtisam-Abdullah <ibtisam.abdullah998@gmail.com>
2025-04-14 03:42:52 +0400

    Update cake_candles.py

--- cake_candles.py
+++ cake_candles.py
@@ -1,4 +1,10 @@
 
+"""
+Problem of the day: Birthday Cake Candles.
+Count how many Candles are the tallest
+candles = [4,1,4,3] ==> The tallest candles are 4 unit high, there are 2 candles with this height, return the number of Candles that are tallest.
+"""
+
 def Count_tallest_candles(candles_lengths):
     lengths_list= candles_lengths.split()
     lengths_list= [int(x) for x in lengths_list]
